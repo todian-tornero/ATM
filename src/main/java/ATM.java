e0633742f49e6092dc96087818167ab3c59993d2
todian-tornero <76249448+todian-tornero@users.noreply.github.com>
2021-02-01 12:37:16 +0300

    Написал приветствие и запрос ПИН-кода

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,7 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ATM implements OperationATM{
+    public boolean cardOnATM = false;
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         ATM atm = new ATM();
         atm.greeting();
+        System.out.println("***********************************");
+        System.out.println("Вставить карту? Y/N");
+        System.out.println("***********************************");
+        try {
+            if ("Y".equals(reader.readLine())) {
+                atm.cardOnATM = true;
+                atm.requestPin();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -12,6 +31,6 @@ public class ATM implements OperationATM{
 
     @Override
     public void requestPin() {
-
+        System.out.println("Введите PIN-код");
     }
 }
